82aa4f5cfcab4036d5114d8a5377a23953124531
BuiThuanIT02 <thuanbui2k215@gmail.com>
2024-01-24 10:09:54 +0700

    update vnpay

--- Web_Sach/Controllers/CartController.cs
+++ Web_Sach/Controllers/CartController.cs
@@ -7,6 +7,8 @@ using Web_Sach.Models;
 using Web_Sach.Models.EF;
 using Web_Sach.Session;
 using System.Web.Script.Serialization;
+using System.Configuration;
+using Web_Sach.Models.Payments;
 
 namespace Web_Sach.Controllers
 {
@@ -236,7 +238,7 @@ namespace Web_Sach.Controllers
 
 
         [HttpPost]
-        public ActionResult Payment(string TenKH, string Mobile, string Address, string Email, int total)
+        public ActionResult Payment(string TenKH, string Mobile, string Address, string Email, int total, string TypePayment,string TypePaymentVN)
         {
             var sessionUser = (UserLoginSession)Session[SessionHelper.USER_KEY];
             WebSachDb db = new WebSachDb();
@@ -273,6 +275,21 @@ namespace Web_Sach.Controllers
             //TempData["Order"] = order;
             //TempData["OrderDetail"] = orderDetail;
             Session[SessionHelper.CART_KEY] = null;// đặt hàng giỏ hàng sẽ trống
+            string url ;
+            // thanh toasn vnpay
+            if (TypePayment !=  "1")
+            {
+                url = UrlPayment(int.Parse(TypePaymentVN), order.ID);
+            }
+            
+
+
+
+
+
+
+
+
             return Redirect("/hoan-thanh");
 
 
@@ -318,6 +335,53 @@ namespace Web_Sach.Controllers
 
 
 
+        // thanh toán vnpay
+        public string UrlPayment(int TypePaymentVN, int orderCode)
+        {
+            var urlPayment = "";
+            var order = db.DonHangs.FirstOrDefault(x => x.ID == orderCode);
+            //Get Config Info
+            string vnp_Returnurl = ConfigurationManager.AppSettings["vnp_Returnurl"]; //URL nhan ket qua tra ve 
+            string vnp_Url = ConfigurationManager.AppSettings["vnp_Url"]; //URL thanh toan cua VNPAY 
+            string vnp_TmnCode = ConfigurationManager.AppSettings["vnp_TmnCode"]; //Ma định danh merchant kết nối (Terminal Id)
+            string vnp_HashSecret = ConfigurationManager.AppSettings["vnp_HashSecret"]; //Secret Key
+
+            //Build URL for VNPAY
+            VnPayLibrary vnpay = new VnPayLibrary();
+            var Price = (long)order.TongTien *100;
+            vnpay.AddRequestData("vnp_Version", VnPayLibrary.VERSION);
+            vnpay.AddRequestData("vnp_Command", "pay");
+            vnpay.AddRequestData("vnp_TmnCode", vnp_TmnCode);
+            vnpay.AddRequestData("vnp_Amount", Price.ToString()); //Số tiền thanh toán. Số tiền không mang các ký tự phân tách thập phân, phần nghìn, ký tự tiền tệ. Để gửi số tiền thanh toán là 100,000 VND (một trăm nghìn VNĐ) thì merchant cần nhân thêm 100 lần (khử phần thập phân), sau đó gửi sang VNPAY là: 10000000
+            if (TypePaymentVN ==1)
+            {
+                vnpay.AddRequestData("vnp_BankCode", "VNPAYQR");
+            }
+            else if (TypePaymentVN == 2)
+            {
+                vnpay.AddRequestData("vnp_BankCode", "VNBANK");
+            }
+            else if (TypePaymentVN == 3)
+            {
+                vnpay.AddRequestData("vnp_BankCode", "INTCARD");
+            }
+
+            vnpay.AddRequestData("vnp_CreateDate", order.NgayDat.Value.ToString("yyyyMMddHHmmss"));
+            vnpay.AddRequestData("vnp_CurrCode", "VND");
+            vnpay.AddRequestData("vnp_IpAddr", Utils.GetIpAddress());
+            vnpay.AddRequestData("vnp_Locale", "vn");
+
+            vnpay.AddRequestData("vnp_OrderInfo", "Thanh toan don hang:" + order.ID);
+            vnpay.AddRequestData("vnp_OrderType", "other"); //default value: other
+
+            vnpay.AddRequestData("vnp_ReturnUrl", vnp_Returnurl);
+            vnpay.AddRequestData("vnp_TxnRef", order.ID.ToString()); // Mã tham chiếu của giao dịch tại hệ thống của merchant. Mã này là duy nhất dùng để phân biệt các đơn hàng gửi sang VNPAY. Không được trùng lặp trong ngày
+
+
+           urlPayment = vnpay.CreateRequestUrl(vnp_Url, vnp_HashSecret);
+            //log.InfoFormat("VNPAY URL: {0}", paymentUrl);
+            //Response.Redirect(paymentUrl);
+            return urlPayment;
 
 
 
@@ -326,6 +390,22 @@ namespace Web_Sach.Controllers
 
 
 
+
+
+
+
+
+        }
+
+
+
+
+
+
+        public ActionResult VnpayReturn()
+        {
+            return View();
+        }
 
 
 
