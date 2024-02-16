a0c5db1dafa8f20d3cc93267417b5ad8f8bc31e5
BuiThuanIT02 <thuanbui2k215@gmail.com>
2024-02-16 16:02:29 +0700

    update filter Product Top Hot

--- Web_Sach/Controllers/ViewAllController.cs
+++ Web_Sach/Controllers/ViewAllController.cs
@@ -19,7 +19,7 @@ namespace Web_Sach.Controllers
             var query = FilterByPrice(filterOb);// lấy ra ds sản phẩm theo danh mục
             if (!query.Any())
             {// Any -> true có phần tử
-                return RedirectToAction("Index", "Home");
+                return RedirectToAction("Index", "Erro");
             }   
             model = query;
 
@@ -100,7 +100,46 @@ namespace Web_Sach.Controllers
 
             return PartialView("_FillerProductViewAll", model.ToList());
         }
-      
+        public ActionResult _FillerProductTopHot(string filterOb, string sortBy, int page = 1, int pageSize = 3)
+        {// lọc sản phẩm xong rồi sắp xếp
+            var query = FilterByPrice(filterOb);// lấy ds sản phẩm đã lọc
+            IQueryable<Sach> sortProduct = db.Saches;
+            IQueryable<Sach> model = db.Saches;
+            var totalItem = 0;
+            if (string.IsNullOrEmpty(sortBy))
+            {
+                model = query;
+                totalItem = model.Count();
+                model = model.OrderByDescending(x => x.Price).Skip((page - 1) * pageSize).Take(pageSize);
+            }
+            else
+            {
+                sortProduct = SortProductViewAll(sortBy, query);// gán query cho sortProduct
+                model = sortProduct;
+                totalItem = model.Count();
+                model = model.Skip((page - 1) * pageSize).Take(pageSize);
+            }
+
+            var totalPage = 0;
+
+            var product = new List<Sach>();
+            int maxPage = 10;
+
+            totalPage = (int)Math.Ceiling((double)totalItem / pageSize);// lamf tron leen
+            ViewBag.filterOb = filterOb;
+            ViewBag.sortBy = sortBy;
+            ViewBag.totalRecord = totalItem;
+            ViewBag.PageSize = pageSize;
+            ViewBag.maxPage = maxPage;
+            ViewBag.page = page;
+            ViewBag.totalPage = totalPage;
+            ViewBag.First = 1;
+            ViewBag.Last = totalPage;
+            ViewBag.Next = page + 1;
+            ViewBag.Prev = page - 1;
+
+            return PartialView("_FillerProductTopHot", model.ToList());
+        }
         public IQueryable<Sach> SortProductViewAll(string sort_by, IQueryable<Sach> filter)
         {
             var query = filter;
@@ -128,7 +167,29 @@ namespace Web_Sach.Controllers
 
             return query;
         }
+        public IQueryable<Sach> SortProductTopHot(string sort_by, IQueryable<Sach> filter)
+        {
+            var query = filter;
+            switch (sort_by)
+            {
+                case "a-z":
 
+                    query = query.OrderBy(x => x.Name);
+                    break;
+                case "z-a":
+
+                    query = query.OrderByDescending(x => x.Name);
+                    break;
+
+                case "cunhat":
+
+                    query = query.OrderBy(x => x.NgayCapNhat);
+                    break;
+
+            }
+
+            return query;
+        }
         [HttpGet]
         public IQueryable<Sach> FilterByPrice(string filterOb)
         {
@@ -176,30 +237,61 @@ namespace Web_Sach.Controllers
             }
             return query;
         }
-
-        public ActionResult TopHot(string sort_by = "", int page = 1, int pageSize = 3)
+        public IQueryable<Sach> FilterByTopHot(string filterOb)
         {
-            IQueryable<Sach> model = db.Saches;
-            ViewBag.sortBy = sort_by;// để lưu giá trị vào từng trang
+            IQueryable<Sach> query = db.Saches.OrderBy(x => x.Quantity);
 
-            var query = model.Where(x => x.NgayCapNhat <= DateTime.Now);
-            switch (sort_by)
+            if (filterOb != null)
             {
-                case "tang":
-                    query = query.OrderBy(x => x.Price);
-                    break;
-                case "giam":
-                    query = query.OrderByDescending(x => x.Price);
-                    break;
-                case "moinhat":
-                    //query = query.OrderBy(x => x.TopHot > DateTime.Now);
-                    break;
-                default:
-                    query = query.OrderBy(x => x.Name);
-                    break;
+                var filter = new JavaScriptSerializer().Deserialize<List<string>>(filterOb);
+
+                foreach (var item in filter)
+                {
+                    switch (item)
+                    {
+                        case "100000":
+                            {
+                                query = query.Where(x => x.Price <= 100000);
+                                break;
+                            }
+
+                        case "100000-200000":
+                            {
+                                query = query.Where(x => x.Price > 100000 && x.Price <= 200000);
+                                break;
+                            }
+                        case "200000-300000":
+                            {
+                                query = query.Where(x => x.Price > 200000 && x.Price <= 300000);
+                                break;
+
+                            }
+                        case "300000-400000":
+                            {
+                                query = query.Where(x => x.Price > 300000 && x.Price <= 400000);
+                                break;
+
+                            }
+                        case "400000":
+                            {
+                                query = query.Where(x => x.Price > 400000);
+                                break;
+                            }
+
+                    }
+                }
             }
+            return query;
+        }
+        public ActionResult TopHot(string filterOb, int page = 1, int pageSize = 3)
+        {
 
-
+            IQueryable<Sach> model = db.Saches;
+            var query = FilterByTopHot(filterOb);// lấy ra ds sản phẩm theo danh mục
+            if (!query.Any())
+            {// Any -> true có phần tử
+                return RedirectToAction("Index", "Erro");
+            }
             model = query;
 
             var totalItem = model.Count(); // số lượng bản ghi
---
+++ Web_Sach/Scripts/Client/Controller/filterProductTopHot.js
@@ -0,0 +1,59 @@
+﻿var filterProduct = {
+    init: function () {
+        filterProduct.removeEventListener();
+    },
+    removeEventListener: function () {
+        $('.sidebar_sort input[type="checkbox"]').change(function () {
+            let selectedFilter = [];
+
+            $('.sidebar_sort input[type="checkbox"]:checked').each(function () {
+                selectedFilter.push($(this).val());
+            })// theo dõi input khi đc chọn
+            $.ajax({
+                type: "GET",
+                url: '/ViewAll/_FillerProductTopHot',
+                data:
+                {
+                    filterOb: JSON.stringify(selectedFilter)
+                }
+                ,
+
+                success: function (res) {
+                    $('#product-tophot').html(res);
+
+
+                }
+
+            })// end ajax
+        })// theo dõi all input
+
+        $('#select_Sort').change(function () {
+
+            let selectedFilter = [];
+            $('.sidebar_sort input[type="checkbox"]:checked').each(function () {
+                selectedFilter.push($(this).val());
+            })// theo dõi input khi đc chọn
+
+            let sortChoose = $('#select_Sort').val();
+
+            $.ajax({
+                type: "GET",
+                url: '/ViewAll/_FillerProductTopHot',
+                data:
+                {
+                    filterOb: JSON.stringify(selectedFilter),
+                    sortBy: sortChoose
+                }
+                ,
+
+                success: function (res) {
+                    $('#product-tophot').html(res);
+
+
+                }
+
+            })// end ajax
+        })// theo dõi thay đổi của selection
+    }
+}
+filterProduct.init();
